de37342e159a64084f0bb0903c4e8366dd02e9c9
Ashab-Asir <ashabasir101@gmail.com>
2025-05-12 11:33:59 +0600

    Added Header all components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
-import React from "react";
-
+import moment from "moment/moment";
+import logo from "../assets/logo.png";
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col justify-center items-center gap-2">
+      <div className="logo">
+        <img className="w-[300px]" src={logo} alt="logo" />
+      </div>
+      <h2 className="text-gray-400">Journalism Without Fear Or Favour</h2>
+      <p>{moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</p>
+    </div>
+  );
 };
 
 export default Header;
